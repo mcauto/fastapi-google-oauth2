0769f8886459b448e1fca3fa08783b3a6655566b
Mincheol Kim <mcauto@naver.com>
2020-07-22 18:59:23 +0900

    token 검증 추가

--- auth.py
+++ auth.py
@@ -2,7 +2,9 @@
 https://accounts.google.com/.well-known/openid-configuration
 """
 import os
+from dataclasses import asdict, dataclass
 from http import HTTPStatus
+from typing import Optional
 
 import requests
 from fastapi import Depends, Security
@@ -19,12 +21,28 @@ ALGORITHMS = ["RS256"]
 USER_INFO_URL = (
     "https://www.googleapis.com/oauth2/v2/userinfo?access_token={access_token}"
 )
+TOKEN_INFO_URL = (
+    "https://oauth2.googleapis.com/tokeninfo?access_token={access_token}"
+)
 
 
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
+
+@dataclass
+class GoogleIDToken:
+    azp: str = ""
+    aud: str = ""
+    sub: str = ""
+    scope: str = ""
+    exp: str = ""
+    expires_in: str = ""
+    email: str = ""
+    email_verified: str = ""
+    access_type: str = ""
 
 
 # Define a Authorization scheme specific to our Auth0 config
@@ -38,6 +56,20 @@ auth0_scheme = OAuth2AuthorizationCodeBearer(
 async def get_current_user(
     security_scopes: SecurityScopes, token: str = Security(auth0_scheme)
 ):
+    if not verify(token):
+        raise AuthError(
+            error="유효하지 않은 토큰입니다", status_code=HTTPStatus.UNAUTHORIZED
+        )
     response = requests.get(USER_INFO_URL.format(access_token=token))
     if response.status_code == HTTPStatus.OK:
         return response.json()
+
+
+def verify(token: str) -> bool:
+    response = requests.get(TOKEN_INFO_URL.format(access_token=token))
+    google_id_token: GoogleIDToken = (
+        GoogleIDToken(**response.json())
+        if response.status_code == HTTPStatus.OK
+        else GoogleIDToken(expires_in="0")
+    )
+    return int(google_id_token.expires_in) > 0
